6c829e7e366a2ebf6ffdef7f62348d188907ef80
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-11-14 15:54:49 -0300

    add ccodigo aula 01

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Disciplina from "./Disciplina.tsx";
+
+export default function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I"/>
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
+    </>
+  );
+}
